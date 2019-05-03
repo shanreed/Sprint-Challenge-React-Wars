6114ce13373de46e113dc9440ccf295068c98109
shanreed <srtech5set@gmail.com>
2019-05-03 09:52:57 -0700

    added some styles, completed MVP

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,25 +1,18 @@
 import React from 'react';
+import './Characters.css';
 
 const Character = (props) => {
     return (
+        <div className = 'character-box'>
         <div className = 'character-info'>
-            
-            <div className = 'character-box'>
                 <h3 className = 'character'>{props.character.name}</h3>
-                <p>{props.character.birth_year}</p>
-                <p>{props.character.gender}</p>
-                <p>{props.character.hair_color}</p>
-                <p>{props.character.eye_color}</p>
-                <p>{props.character.skin_color}</p>
-                <p>{props.character.mass}</p>
-                
-                
-                
-                
-                
-                
-            </div>
-                
+                <p className = 'green'>Was born {props.character.birth_year}</p>
+                <p className = 'yellow'>Is a {props.character.gender}</p>
+                <p className = 'red'>Hair color is {props.character.hair_color}</p>
+                <p className = 'orange'>Eye color is {props.character.eye_color}</p>
+                <p className = 'purple'>Skin color is {props.character.skin_color}</p>
+                <p className = 'brown'>Mass is {props.character.mass}</p>
+        </div>
         </div>
     );
 }
